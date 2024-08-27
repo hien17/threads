11e9d72e99f1e646a3c1ce5b7511e74568bb67f3
Phạm Nhật Hưng <nhathung040204@gmail.com>
2024-08-27 19:13:34 +0700

    update onboarding for (auth) in app and account profile for forms in components

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,21 +1,20 @@
-import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs/server";
+import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
   const user = await currentUser();
 
-  const userInfo = {
+  const userInfo = {}
 
-  }
-  
-  const userData = {
-    id: user?.id,
-    objectId: userInfo?.id, 
-    username: userInfo?.username || user?.username,
-    name: userInfo?.name || user?.firstName || "",
-    bio: userInfo?.bio || "",
-    image: userInfo?.image || user?.imageUrl,
-  }
+  // const userData = {
+  //   id: user.id,
+  //   objectId: userInfo?._id,
+  //   username: userInfo?.username || user?.username,
+  //   name:  userInfo?.name || user?.firstName || "",
+  //   bio: userInfo?.bio || "",
+  //   image: userInfo?.image || user.imageUrl,
+  // };
+
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
@@ -24,14 +23,12 @@ async function Page() {
       </p>
 
       <section className='mt-9 bg-dark-2 p-10'>
-        <AccountProfile
+        {/* <AccountProfile 
         user={userData}
-        btnTitle="Continue"
-        />
+        btnTitle='Continue' /> */}
       </section>
     </main>
   );
 }
-    
 
-export default Page
+export default Page;
